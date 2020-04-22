53497b6d7b64e913b313a6c15587b9545bc73a6c
ryo <ryo8857@gmail.com>
2020-04-22 16:23:27 +0900

    Introduce prittier to the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
 interface SquarePropsInterface {
-  value: number;
+  value: number
   onClick: () => void
 }
 
-function Square(props: SquarePropsInterface){
+function Square(props: SquarePropsInterface) {
   let stoneColor: string
   switch (props.value) {
     case 1:
-      stoneColor = "black-stone"
+      stoneColor = 'black-stone'
       break
     case -1:
-      stoneColor = "white-stone"
+      stoneColor = 'white-stone'
       break
     default:
-      stoneColor = "no-stone"
+      stoneColor = 'no-stone'
       break
   }
   return (
-    <button
-      className="square"
-      onClick={props.onClick}
-    >
+    <button className="square" onClick={props.onClick}>
       <div className={stoneColor} />
     </button>
-  );
+  )
 }
 
-interface BoardPropsInterface{
+interface BoardPropsInterface {
   squares: Array<number>[]
 }
 
-interface BoardStateInterface{
-  squares: Array<number>[];
-  xIsNext: boolean;
+interface BoardStateInterface {
+  squares: Array<number>[]
+  xIsNext: boolean
 }
 
 class Board extends React.Component<BoardPropsInterface, BoardStateInterface> {
   constructor(props: BoardPropsInterface) {
-    super(props);
+    super(props)
     this.state = {
       squares: boardInit(),
       xIsNext: false,
@@ -57,17 +54,22 @@ class Board extends React.Component<BoardPropsInterface, BoardStateInterface> {
     let reversedFlag = false
     let reversedLineCount = 0
     const directionList = [
-                          [-1, -1], [ 0, -1], [ 1, -1],
-                          [-1,  0],           [ 1,  0],
-                          [-1,  1], [ 0,  1], [ 1,  1]
-                        ]
+      [-1, -1],
+      [0, -1],
+      [1, -1],
+      [-1, 0],
+      [1, 0],
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+    ]
     for (const direction of directionList) {
       const squaresBackUp = JSON.parse(JSON.stringify(squares))
       let x = j
       let y = i
       isUserStone = false
       reversedFlag = false
-      while (true){
+      while (true) {
         x += direction[0]
         y += direction[1]
         if (x < 0 || 7 < x || y < 0 || 7 < y) {
@@ -83,7 +85,7 @@ class Board extends React.Component<BoardPropsInterface, BoardStateInterface> {
         squares[y][x] = stoneColor
         reversedFlag = true
       }
-      if ((!reversedFlag || !isUserStone)) {
+      if (!reversedFlag || !isUserStone) {
         squares = squaresBackUp
       } else {
         reversedLineCount += 1
@@ -96,46 +98,41 @@ class Board extends React.Component<BoardPropsInterface, BoardStateInterface> {
     return false
   }
 
-  handleClick(i: number, j:number) {
-    const stoneColor = this.state.xIsNext ? 1 : -1;
-    for (let dir=0; dir<9; dir++) {
+  handleClick(i: number, j: number) {
+    const stoneColor = this.state.xIsNext ? 1 : -1
+    for (let dir = 0; dir < 9; dir++) {
       const newSquares = this.reverseEightLine(i, j, stoneColor)
       if (!newSquares) {
         return
       }
       this.setState({
         squares: newSquares,
-        xIsNext: !this.state.xIsNext
-      });
+        xIsNext: !this.state.xIsNext,
+      })
     }
   }
 
-  renderSquare(i: number, j:number) {
-    return <Square
-      value={this.state.squares[i][j]}
-      onClick={() => this.handleClick(i, j)}
-    />;
+  renderSquare(i: number, j: number) {
+    return <Square value={this.state.squares[i][j]} onClick={() => this.handleClick(i, j)} />
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status: string;
+    const winner = calculateWinner(this.state.squares)
+    let status: string
     if (winner) {
-      status = winner;
+      status = winner
     } else {
-      status = "Next Player: " + (this.state.xIsNext ? "黒" : "白");
+      status = 'Next Player: ' + (this.state.xIsNext ? '黒' : '白')
     }
 
     return (
       <div>
         <div className="status">{status}</div>
-        { [0,1,2,3,4,5,6,7].map((i) =>
-          <div className="board-row">
-            { [0,1,2,3,4,5,6,7].map((j) => this.renderSquare(i, j))}
-          </div>
-        )}
+        {[0, 1, 2, 3, 4, 5, 6, 7].map((i) => (
+          <div className="board-row">{[0, 1, 2, 3, 4, 5, 6, 7].map((j) => this.renderSquare(i, j))}</div>
+        ))}
       </div>
-    );
+    )
   }
 }
 
@@ -144,54 +141,51 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={boardInit()}/>
+          <Board squares={boardInit()} />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
-    );
+    )
   }
 }
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Game />, document.getElementById('root'))
 
 function countStone(squares: Array<number>[]) {
-  let black: number = 0;
-  let white: number = 0;
+  let black: number = 0
+  let white: number = 0
 
   for (const values of squares) {
     for (const value of values) {
       if (value === 1) {
-        black++;
+        black++
       } else if (value === -1) {
-        white++;
+        white++
       }
     }
   }
 
-  console.log("black=", black)
-  console.log("white=", white)
-  return [black, white];
+  console.log('black=', black)
+  console.log('white=', white)
+  return [black, white]
 }
 
 function boardInit() {
   return [
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,1,-1,0,0,0],
-            [0,0,0,-1,1,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-          ]
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, -1, 0, 0, 0],
+    [0, 0, 0, -1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ]
 }
 
 function calculateWinner(squares: Array<number>[]) {
@@ -199,13 +193,12 @@ function calculateWinner(squares: Array<number>[]) {
   const stoneNumber = black + white
   if (stoneNumber == 64) {
     if (black < white) {
-      return "白の勝利！"
+      return '白の勝利！'
     }
     if (black === white) {
-      return "引き分け"
+      return '引き分け'
     }
-    return "黒の勝利！"
-
+    return '黒の勝利！'
   }
-  return null;
+  return null
 }
